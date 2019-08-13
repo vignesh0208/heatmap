51b4c1854bb6a13e9fcc706a3a8c29af4a0df889
vignesh0208 <v19ne5h1994@gmail.com>
2019-08-13 09:26:45 +0530

    12 aug 2019

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var Config = require('./config.js');
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
+var express = require ('express');
+var nunjucks  = require('nunjucks');
+var port = 3000;
+var app = express();
+app.use('/public', express.static('public'));
+
+var env = nunjucks.configure(['views/'], { 
+    autoescape: true, 
+    express: app
+});
+
+app.get('/', (req, res) => {
     res.render('index.html', {
-        title: "Hello world",
-        page: 'home'
-    }); 
-})
- 
-app.listen(3000)
+      title: Config['SEO']['Home']['title'],
+      page: 'home'
+    });    
+});
+
+app.get('/about', function(req, res){
+    res.render('pages/about.html', {
+      title: Config['SEO']['About']['title'],
+      page: 'about',
+    });
+});
+
+app.listen(port, function() {
+    console.log('Example app listening on port... http://localhost:'+ port +'');
+});
